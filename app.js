1578d56d9b1dded8be41568d595a499bf627e38c
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-20 11:29:04 +0300

    loodud projekti failid

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ console.log("Hello world!")
 
 //variables
 //let
+//data types
+//string
 let name = "Viktor Lumiste"
 name = "New name"
 
-//const
-const surename = "Lumiste"
+let heahinne = 5
+console.log(heahinne)
+console.log(typeof heahinne)
+
+let percent = 50.3
+console.log(percent)
+console.log(typeof percent)
+
+let ishere = true // false
+console.log(ishere)
+console.log(typeof ishere)
+
 
 //arrays
 const numbers = [1,2,3,4,5]
 console.log(numbers)
 numbers.push(6)
-console.log(numbers)
+console.log(typeof numbers)
+
+//const
+const surename = "Lumiste"
 
 //objects
 const person = {
@@ -24,3 +39,19 @@ console.log(person.surename)
 console.log(person.firstname)
 person.department = "ITA"
 console.log(person)
+console.log(typeof person)
+
+//null
+const airplane= null
+console.log(airplane)
+console.log(typeof airplane)
+
+//symbol
+const symbol = Symbol()
+console.log(symbol)
+console.log(typeof symbol)
+
+//undefined
+let test
+console.log(test)
+console.log(typeof test)
