2003f813135ae57062ad32d0e6823b750d27be7c
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-20 11:18:15 +0300

    loodud projekti failid

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("Hello world!")
+
+//variables
+//let
+let name = "Viktor Lumiste"
+name = "New name"
+
+//const
+const surename = "Lumiste"
+
+//arrays
+const numbers = [1,2,3,4,5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+//objects
+const person = {
+    firstname: "Viktor",
+    surename: "Lumiste"
+}
+console.log(person)
+console.log(person.surename)
+console.log(person.firstname)
+person.department = "ITA"
+console.log(person)
