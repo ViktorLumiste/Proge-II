6b9303bf7206d4d077d3ef73618a1f469cf4f04a
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-22 13:17:59 +0300

    ajad

---
+++ ajad.js
@@ -0,0 +1,38 @@
+const today = new Date()
+
+
+
+
+
+
+let val
+
+
+
+
+
+
+
+val = today
+val = today.getMonth() // 0 on jaanuar
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getTime()
+
+val = new Date("09/22/2021")
+val = new Date("September 23 2022")
+
+val = new Date()
+val.setDate(23)
+val.setFullYear(2023)
+
+
+
+
+
+
+
+console.log(val)
