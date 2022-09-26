cee8d91aa0c46d8f9119d20e3f46e7e5bc002feb
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-26 13:51:19 +0300

    document

--- app.js
+++ app.js
@@ -1,57 +1,17 @@
-console.log("Hello world!")
+let val
 
-//variables
-//let
-//data types
-//string
-let name = "Viktor Lumiste"
-name = "New name"
+val = document
+val = document.all[6]
+val = document.all.length
 
-let heahinne = 5
-console.log(heahinne)
-console.log(typeof heahinne)
+val = document.head
+val = document.body
 
-let percent = 50.3
-console.log(percent)
-console.log(typeof percent)
+val = document.doctype
+val = document.domain
+val = document.URL
+val = document.characterSet
+val = document.contentType
 
-let ishere = true // false
-console.log(ishere)
-console.log(typeof ishere)
-
-
-//arrays
-const numbers = [1,2,3,4,5]
-console.log(numbers)
-numbers.push(6)
-console.log(typeof numbers)
-
-//const
-const surename = "Lumiste"
-
-//objects
-const person = {
-    firstname: "Viktor",
-    surename: "Lumiste"
-}
-console.log(person)
-console.log(person.surename)
-console.log(person.firstname)
-person.department = "ITA"
-console.log(person)
-console.log(typeof person)
-
-//null
-const airplane= null
-console.log(airplane)
-console.log(typeof airplane)
-
-//symbol
-const symbol = Symbol()
-console.log(symbol)
-console.log(typeof symbol)
-
-//undefined
-let test
-console.log(test)
-console.log(typeof test)
+val = document.forms
+console.log(val)
