64ea1e5885cde26afbda3ddbb49e917f832357da
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-26 13:59:17 +0300

    document

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ val = document.contentType
 
 val = document.forms
 val = document.forms[0]
+val = document.forms[0].method
+val = document.forms[0].action
+val = document.forms[0].id
+
+val = document.links
+val = document.links[0]
+val = document.links[0].id
+val = document.links[0].className
+val = document.links[0].classList
+val = document.links[0].classList[2]
+
+val=document.scripts
+val=document.scripts[0]
+val=document.scripts[1]
+val=document.scripts[1].getAttribute('src')
 console.log(val)
