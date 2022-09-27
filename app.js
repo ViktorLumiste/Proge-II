41e01cd2a2bfabbb745a7ee567996024958ea72a
ViktorLumiste <viktor.lumiste@gmail.com>
2022-09-27 09:24:07 +0300

    DOM selector for single and multiple elements

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 let val
 
-val = document
-val = document.all[6]
-val = document.all.length
+// val = document.getElementById("task-form")
+// val = document.getElementById("task-form").id
+// val = document.getElementById("task-form").className
 
-val = document.head
-val = document.body
+const taskTitle = document.getElementById("task-title")
 
-val = document.doctype
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
+taskTitle.style.background = "#333"
+taskTitle.style.color = "#fff"
+taskTitle.style.padding = "15px"
 
-val = document.forms
-val = document.forms[0]
-val = document.forms[0].method
-val = document.forms[0].action
-val = document.forms[0].id
+taskTitle.textContent= "Defined tasks"
+taskTitle.innerText= "Favourite tasks"
+taskTitle.innerHTML= "<b style='color:red'>Favourite tasks</b>"
 
-val = document.links
-val = document.links[0]
-val = document.links[0].id
-val = document.links[0].className
-val = document.links[0].classList
-val = document.links[0].classList[2]
+val = document.querySelector("#task-title")
+val = document.querySelector(".card-title")
+val = document.querySelector('ul')
+val = document.querySelector('li')
 
-val=document.scripts
-val=document.scripts[0]
-val=document.scripts[1]
-val=document.scripts[1].getAttribute('src')
+const task = document.querySelector('li')
+task.style.color = "green"
+
+// val = document.querySelector('li:last-child')
+// document.querySelector('li:nth-child(odd)').style.background = "#ccc"
+// document.querySelector('li:nth-child(even)').style.background = "#f4f4f4"
+
+val = document.querySelectorAll("li")
+odd_li = document.querySelectorAll("li:nth-child(odd)")
+odd_li.forEach((li)=>{
+    li.style.background = "#ddd"
+})
+
+
+
 console.log(val)
+console.log(taskTitle)
